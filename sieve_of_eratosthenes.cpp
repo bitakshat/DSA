9325e919a11e04c04da5302b68b7bc3bc57b8416
Akshat2205 <akshatpal1232@gmail.com>
2020-06-07 20:24:43 +0530

    not a DS sieve of eratosthenes added for future use

---
+++ sieve_of_eratosthenes.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+void sieve_of_eratosthenes( int n ) {
+	bool prime[n+1];
+	memset(prime, true, sizeof(prime));
+	int p = 2;
+
+	for( int i=p; i<=n; ++i ) {
+		if( prime[i] == true) {
+			for( int j=i*i; j<=n; j+=i ) {
+				prime[j] = false;
+			}
+		}
+	}
+	std::cout <<"Prime numbers in range " << n << ": ";
+	/*Printing all prime numbers*/
+	for( int x=2; x<=n; x++ ) {
+		if( prime[x] ){
+			std::cout << x << " ";
+		}
+	}
+}
+
+int main( void ) {
+	sieve_of_eratosthenes(10);
+	return 0;
+}
