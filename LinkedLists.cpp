ec7ae813356f18036cc8df96e565c0e41c9e24fa
Akshat2205 <akshatpal1232@gmail.com>
2020-06-15 19:24:51 +0530

    Linked list added

--- LinkedLists.cpp
+++ LinkedLists.cpp
@@ -1,42 +1,33 @@
-/*Linked List*/
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
-struct Node {
+struct node{
 	int data;
-	struct Node* next;
+	struct node* next;
 };
-struct Node* head = NULL;
-void insert( int new_data ) {
-	//struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
-	struct Node* new_node = new Node();
-	new_node -> data = new_data;
+
+struct node* head = NULL;
+void insert(int new_data) {
+	struct node* new_node = (struct node*) malloc(sizeof(struct node));
+	new_node -> data = new_data;    
 	new_node -> next = head;
 	head = new_node;
 }
+
 void display() {
-	struct Node* ptr;
+	struct node* ptr;
 	ptr = head;
 	while( ptr != NULL ) {
 		std::cout << ptr -> data << " ";
 		ptr = ptr -> next;
 	}
 }
-//function to delete contents of node
-void delte(int data) {
-	struct Node* del;
-}
 
-int main( void ) {
-	for( int x=1; x<=5; ++x ) {
+int main(void) {
+
+	std::cout <<"Inserting data: ";
+	for( int x=0; x<=5; x++ ) {
 		insert(x);
-		if( x == 2 ) {
-			delete_data(x);
-		}
-		else {
-			continue;
-		}
 	}
-	std::cout <<"Contents of Linked list: ";
 	display();
 	return 0;
 }
