652aa07de93a6824bfdac1c7fd639b404d915f30
Akshat2205 <akshatpal1232@gmail.com>
2020-06-23 23:24:57 +0530

    stack DS added

---
+++ stack.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+
+#define MAX 10
+int size = 0;
+
+/*creating a stack*/
+ struct stack {
+    int items[MAX];
+    int top;
+};
+
+typedef struct stack st;
+void 
+createEmptyStack( st *s ) {
+    s -> top = -1;
+}
+
+/* check if the stack is full */
+int
+isFull( st *s ) {
+    if(s->top == MAX - 1)
+        return 1;
+    else
+	return 0;
+}
+
+/* check if the stack is empty */
+int 
+isEmpty( st *s ) {
+    if(s->top == -1)
+        return 1;
+    else
+	return 0;
+}
+
+/* push elemnts to the stack */
+void 
+push( st* s, int new_item ) {
+    if( isFull(s) ) {
+        std::cout << "Stack is already full!" << std::endl;
+        return 0;
+    }
+    else {
+        s->top++;
+        s->items[s->top] = new_item;
+    }
+    size++;
+}
+
+
+/* Remove element from stack */
+void
+pop( st *s ){
+    if(isEmpty(s)){
+    	std::cout << "The stack is empty!" << std::endl;
+    }
+    else {
+	std::cout << "Item popped: ";
+	s->top--;
+    }
+    size--;
+    std::endl;
+}
+
+/* Prints elements of stack */
+void 
+printStack( st *s ) {
+    std::cout << "Stack: ";
+    for( int i=0; i<=size; i++ ) {
+        std::cout << s->items[i] << " ";
+    }
+    std::endl;
+}
+
+//driver code
+int
+main( void ) {
+    int ch;
+    st *s = (st*) malloc(sizeof(st));
+
+    createEmptyStack(s);
+    for( int x=0; x<=5; x++) {
+        push(s, x);
+    }
+
+    printStack(s);
+    pop(s);
+    std::cout << "After poppint out!" << '\n';
+    printStack(s);
+}
