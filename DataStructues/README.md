ff83269dad2863cc3230c44da92023b4a0ef388d
Akshat2205 <akshatpal1232@gmail.com>
2020-06-26 20:59:50 +0530

    files recovered

---
+++ DataStructues/README.md
@@ -0,0 +1,3 @@
+###### All the DataStuctures I've learned so far
+
+>Compile using command line: ```g++ -o out <filename.cpp> ./out```
---
+++ DataStructues/vectors.cpp
@@ -0,0 +1,20 @@
+/*The Vectors*/
+#include<iostream>
+#include<vector>
+
+int function( int n ) {
+	std::vector<int> vec;
+
+	for( int i=0; i<=n; ++i ) {
+		vec.push_back(i);
+	}
+	std::vector<int>::iterator it;
+	for( it=vec.begin(); it != vec.end(); it++ ) {
+		std::cout << *it << " ";
+	}
+}
+
+int main(void) {
+	function(10);
+	return 0;
+} 
