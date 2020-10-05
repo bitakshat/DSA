95dc69ac9eac63db86ed5a3cd7a0abeedaa7570a
bitakshat <akshatpal1232@gmail.com>
2020-10-05 22:29:09 +0530

    Update documentation and Directory structure redefined

---
+++ DataStructures/LinkedList/passwordgeneraotr.py
@@ -0,0 +1,17 @@
+from ramdom import *
+
+#liste caractères
+characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+#longueur
+length = int(input('choose the length of your password :'))
+
+#generation
+def mdp (length):
+	mdp = ""
+	for x in range(length):
+		char = length.random
+		print(length)
+
+
+mdp()
