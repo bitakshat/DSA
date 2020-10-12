3b2b22a696eda397bc3a306e78db5fa0132f955a
bitakshat <akshatpal1232@gmail.com>
2020-10-12 20:59:24 +0530

    added hashtable hashing algorithm

--- DataStructures/HashTable/hashtable.cpp
+++ DataStructures/HashTable/hashtable.cpp
@@ -1,5 +1,31 @@
-#include <bits/stdc++.h>
 
-int main( void ) {
-	return 0;
+
+
+
+
+#include <iostream>
+
+int func(int *arr, int size) {
+	int hashArr[size];
+	int index = 0;
+    std::cout << "Hash Array: ";
+    for(int x = 0; x < size; x++) {
+    	// hashArr[x] = arr[x] % size;
+    	index = arr[x] % size;
+    	hashArr[index] = arr[x];
+    	std::cout << hashArr[x] << " ";
+    }
+}
+
+int main() {
+    int array[] = {10, 20, 30, 40, 50};
+   	int size = sizeof(array) / sizeof(array[0]);
+
+   	std::cout << "Original Array: ";
+   	for(int i = 0; i < size; i++) {
+		std::cout << array[i] << " ";
+	}
+    func(array, size);
+    
+    return 0;
 }
