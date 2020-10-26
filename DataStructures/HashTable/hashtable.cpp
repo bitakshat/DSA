4cd0bd9169f0d9d50e9e180a3c6fd29b82794f77
bitakshat <akshatpal132@gmail.com>
2020-10-26 10:24:31 +0530

    added code for hashtable

--- DataStructures/HashTable/hashtable.cpp
+++ DataStructures/HashTable/hashtable.cpp
@@ -1,31 +1,81 @@
 
 
 
-
-
 #include <iostream>
+#include <list>
 
-int func(int *arr, int size) {
-	int hashArr[size];
-	int index = 0;
-    std::cout << "Hash Array: ";
-    for(int x = 0; x < size; x++) {
-    	// hashArr[x] = arr[x] % size;
-    	index = arr[x] % size;
-    	hashArr[index] = arr[x];
-    	std::cout << hashArr[x] << " ";
+class HashTable {
+private:
+    std::list<int> *table;
+    int total_elements;
+
+    // Hash Function to calculate hash for a value
+    int getHash(int key) {
+        return key % total_elements;
+    }
+
+public:
+    // Constructor to create a hashtable with 'n' indices
+    HashTable(int n) {
+        total_elements = n;
+        table = new std::list<int>[total_elements];
+    }
+
+    void insertElement(int key);
+    void removeElement(int key);
+    void printAll();
+};
+
+
+void HashTable::insertElement(int key) {
+    table[getHash(key)].push_back(key);
+}
+
+void HashTable::removeElement(int key) {
+    int x = getHash(key);
+    std::list<int>::iterator i;
+
+    for(i = table[x].begin(); i != table[x].end(); i++) {
+        if(*i == key)
+            std::cout << "[!] Element Not Found!" << std::endl;
+            break;
+        if(i != table[x].end()) 
+            table[x].erase(i);
     }
 }
 
-int main() {
-    int array[] = {10, 20, 30, 40, 50};
-   	int size = sizeof(array) / sizeof(array[0]);
-
-   	std::cout << "Original Array: ";
-   	for(int i = 0; i < size; i++) {
-		std::cout << array[i] << " ";
-	}
-    func(array, size);
-    
-    return 0;
+void HashTable::printAll() {
+    for(int i = 0; i < total_elements; i++) {
+        std::cout << "Index " << i << " : ";
+        
+        for(auto j: table[i])
+            std::cout << j << " => ";
+        std::cout << std::endl;
+    }
 }
+
+// Driver Code
+int main() {
+    HashTable ht(3);
+    int arr[] = {2, 3, 4, 6, 10};
+    for(int i = 0; i < 5; i++) 
+        ht.insertElement(arr[i]);
+
+    std::cout << "..:: Hash Table ::.." << std::endl;
+    ht.printAll();
+    ht.removeElement(4);
+    std::cout << std::endl << "..:: After deleting element ::..";
+    ht.printAll();
+
+    return 0; 
+}
+
+
+
+
+
+
+
+
+
+
